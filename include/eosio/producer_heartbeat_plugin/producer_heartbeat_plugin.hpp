59b18b22d66194b3a719acdf680bb6f48680b39d
Tal Muskal <tal@bancor.network>
2018-08-31 04:17:03 +0000

    fixing compilation

--- include/eosio/producer_heartbeat_plugin/producer_heartbeat_plugin.hpp
+++ include/eosio/producer_heartbeat_plugin/producer_heartbeat_plugin.hpp
@@ -5,7 +5,7 @@
 #pragma once
 #include <appbase/application.hpp>
 #include <eosio/chain_plugin/chain_plugin.hpp>
-
+#include <eosio/producer_plugin/producer_plugin.hpp>
 namespace eosio {
 
 using namespace appbase;
@@ -19,7 +19,8 @@ public:
    producer_heartbeat_plugin();
    virtual ~producer_heartbeat_plugin();
  
-   APPBASE_PLUGIN_REQUIRES()
+   APPBASE_PLUGIN_REQUIRES((producer_plugin)(chain_plugin))
+
    virtual void set_program_options(options_description&, options_description& cfg) override;
  
    void plugin_initialize(const variables_map& options);
--- producer_heartbeat_plugin.cpp
+++ producer_heartbeat_plugin.cpp
@@ -9,16 +9,18 @@
 #include <eosio/chain/abi_serializer.hpp>
 
 
+#include <eosio/utilities/common.hpp>
+
 // HACK TO EXPOSE LOGGER MAP
 
 namespace fc {
    extern std::unordered_map<std::string,logger>& get_logger_map();
 }
 
-const fc::string logger_name("producer_plugin");
-fc::logger _log;
 
 namespace eosio {
+   using namespace eosio::chain;
+   
    static appbase::abstract_plugin& _template_plugin = app().register_plugin<producer_heartbeat_plugin>();
 
 
@@ -28,23 +30,25 @@ class producer_heartbeat_plugin_impl {
       unique_ptr<boost::asio::steady_timer> timer;
       boost::asio::steady_timer::duration timer_period;
       fc::microseconds _keosd_provider_timeout_us;
-      std:string heartbeat_contract;
-      std:string producer_name;
+      std::string heartbeat_contract;
+      std::string producer_name;
       fc::crypto::private_key _hearbeat_private_key;
       chain::public_key_type _hearbeat_public_key;
       
       void send_heartbeat_transaction(){
-            chain::chain_id_type chainid;
-            auto& plugin = _app.get_plugin<chain_plugin>();
+            auto& plugin = app().get_plugin<chain_plugin>();
             auto actor_blacklist_hash = 0;
-            plugin.get_chain_id(chainid);
+            auto chainid = plugin.get_chain_id();
+            auto abi_serializer_max_time = plugin.get_abi_serializer_max_time();
+   
             controller& cc = plugin.chain();
+            abi_serializer eosio_serializer(cc.db().find<account_object, by_name>(heartbeat_contract)->get_abi(), abi_serializer_max_time);
             signed_transaction trx;
             action act;
             act.account = heartbeat_contract;
             act.name = N(hearbeat);
             act.authorization = vector<permission_level>{{producer_name,"hearbeat"}};
-            act.data = eosio_token_serializer.variant_to_binary("create",mutable_variant_object()
+            act.data = eosio_serializer.variant_to_binary("create",mutable_variant_object()
                ("server_version", eosio::utilities::common::itoh(static_cast<uint32_t>(app().version())))
                ("actor_blacklist_hash", eosio::utilities::common::itoh(actor_blacklist_hash))
                ("head_block_num",  cc.fork_db_head_block_num()), 
@@ -54,12 +58,8 @@ class producer_heartbeat_plugin_impl {
             trx.expiration = cc.head_block_time() + fc::seconds(30);
             trx.set_reference_block(cc.head_block_id());
             trx.sign(_hearbeat_private_key, chainid);
-      
-            try {
-               cc.push_transaction( std::make_shared<transaction_metadata>(trx) );
-            } catch (const account_name_exists_exception& ) {
-               // fallback
-            }
+            cc.push_transaction( std::make_shared<transaction_metadata>(trx) , trx.expiration);
+
       }
       void start_timer( ) {
          timer->expires_from_now(timer_period);
@@ -103,19 +103,19 @@ void producer_heartbeat_plugin::plugin_initialize(const variables_map& options) 
          my->heartbeat_contract = options.at( "heartbeat-contract" ).as<std::string>();
       }
       if(options.count("producer-name")){
-         my->producer_name =  options["producer-name"].as<std::vector<std::string>>()
+         my->producer_name =  options["producer-name"].as<std::string>();
       }
       if( options.count("heartbeat-signature-provider") ) {
             const std::string key_spec_pair = options["heartbeat-signature-provider"].as<std::string>();
             
             try {
                auto delim = key_spec_pair.find("=");
-               EOS_ASSERT(delim != std::string::npos, plugin_config_exception, "Missing \"=\" in the key spec pair");
+               EOS_ASSERT(delim != std::string::npos, eosio::chain::plugin_config_exception, "Missing \"=\" in the key spec pair");
                auto pub_key_str = key_spec_pair.substr(0, delim);
                auto spec_str = key_spec_pair.substr(delim + 1);
    
                auto spec_delim = spec_str.find(":");
-               EOS_ASSERT(spec_delim != std::string::npos, plugin_config_exception, "Missing \":\" in the key spec pair");
+               EOS_ASSERT(spec_delim != std::string::npos, eosio::chain::plugin_config_exception, "Missing \":\" in the key spec pair");
                auto spec_type_str = spec_str.substr(0, spec_delim);
                auto spec_data = spec_str.substr(spec_delim + 1);
    
@@ -123,7 +123,7 @@ void producer_heartbeat_plugin::plugin_initialize(const variables_map& options) 
                
                
                if (spec_type_str == "KEY") {
-                  my->_hearbeat_private_key = make_key_signature_provider(private_key_type(spec_data));   
+                  my->_hearbeat_private_key = fc::crypto::private_key(spec_data);
                   my->_hearbeat_public_key = pubkey;   
                } else if (spec_type_str == "KEOSD") {
                   // not supported
@@ -139,15 +139,8 @@ void producer_heartbeat_plugin::plugin_initialize(const variables_map& options) 
 }
 
 void producer_heartbeat_plugin::plugin_startup() {
-   if(fc::get_logger_map().find(logger_name) != fc::get_logger_map().end()) {
-      _log = fc::get_logger_map()[logger_name];
-   }
-   auto& producer = app().get_plugin<producer_plugin>();
    my->start_timer();
-   
-   
-   
-   
+
 }
 
 void producer_heartbeat_plugin::plugin_shutdown() {
