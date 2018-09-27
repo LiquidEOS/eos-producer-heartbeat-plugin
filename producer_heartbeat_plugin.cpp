151ce296b1f2d9860746e9839aac1a89038f9beb
Tal Muskal <tal@bancor.network>
2018-09-27 17:35:52 +0000

    version

--- producer_heartbeat_plugin.cpp
+++ producer_heartbeat_plugin.cpp
@@ -105,7 +105,7 @@ class producer_heartbeat_plugin_impl {
          lock.unlock();
 
          return mutable_variant_object()
-               ("hb_version", "1.1")
+               ("hb_version", "1.1.01")
                ("version", eosio::utilities::common::itoh(static_cast<uint32_t>(app().version())))
                ("version_string", app().version_string())
                ("abl_hash", actor_blacklist_hash)
